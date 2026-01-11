18ab98db13438477c4e4043f637a3276ddc09a73
anmolassi <79657095+anmolassi@users.noreply.github.com>
2026-01-11 13:40:38 +0530

    initial commit

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT ?? 3000;
+  Logger.log("App is working on the PORT:", port);
+  await app.listen(port);
+}
+bootstrap();
